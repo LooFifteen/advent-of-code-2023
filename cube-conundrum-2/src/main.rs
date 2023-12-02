03bf5dae1c75f34dce0912a38d058c50cb09663e
Luis <luis@lu15.dev>
2023-12-02 14:28:23 +0000

    feat: day 2, part 2

--- cube-conundrum-2/src/main.rs
+++ cube-conundrum-2/src/main.rs
@@ -1,18 +1,21 @@
+use std::collections::HashMap;
+
 const INPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/input.txt");
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 enum Colour {
-    Red(u32),
-    Green(u32),
-    Blue(u32)
+    Red,
+    Green,
+    Blue
 }
 
 fn main() {
     // read input from file
     let input = std::fs::read_to_string(INPUT_FILE).expect("Failed to read input file.");
 
-    // initialize sum
-    let mut sum = 0;
+    // initialize sums
+    let mut part_1 = 0;
+    let mut part_2 = 0;
 
     // iterate through each line
     for line in input.lines() {
@@ -31,9 +34,10 @@ fn main() {
 
         // initialize possible variable
         let mut possible = true;
+        let mut minimum_required = HashMap::new();
 
         // iterate through each set
-        'sets: for set in sets {
+        for set in sets {
             // split set by comma
             let hands = set.split(',');
 
@@ -47,31 +51,47 @@ fn main() {
 
                 // convert to Colour
                 let colour = match colour {
-                    "red" => Colour::Red(number),
-                    "green" => Colour::Green(number),
-                    "blue" => Colour::Blue(number),
+                    "red" => Colour::Red,
+                    "green" => Colour::Green,
+                    "blue" => Colour::Blue,
                     _ => panic!("Invalid colour.")
                 };
 
+                // if the number of this colour is more than the minimum required, set the minimum required to this number
+                if let Some(minimum_required) = minimum_required.get_mut(&colour) {
+                    if number > *minimum_required {
+                        *minimum_required = number;
+                    }
+                } else {
+                    minimum_required.insert(colour, number);
+                }
+
                 // check if number of each colour is valid
-                if !match colour {
-                    Colour::Red(number) => number <= 12,
-                    Colour::Green(number) => number <= 13,
-                    Colour::Blue(number) => number <= 14
+                if possible && !match colour {
+                    Colour::Red => number <= 12,
+                    Colour::Green => number <= 13,
+                    Colour::Blue => number <= 14
                 } {
                     // if not, set possible to false and break out of loop
                     possible = false;
-                    break 'sets;
                 }
             }
         }
 
+        // multiply each minimum required number
+        let mut power = 1;
+        for (_, number) in minimum_required {
+            power *= number;
+        }
+        part_2 += power;
+
         // if possible, add number to sum
         if possible {
-            sum += number;
+            part_1 += number;
         }
     }
 
-    // print sum
-    println!("Sum: {}", sum);
+    // print sums
+    println!("Part 1: {}", part_1);
+    println!("Part 2: {}", part_2);
 }
